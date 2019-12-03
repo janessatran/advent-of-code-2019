bb4421397b45729bb74022148ecfdd89ca344e15
Janessa Tran <tranjanessa@gmail.com>
2019-12-02 21:36:47 -0600

    Add part two and organize code into module

--- day-01/day-01.rb
+++ day-01/day-01.rb
@@ -1,15 +1,30 @@
-input_data = File.open("input.txt")
+module Day1
+  class << self
+    def get_data
+      File.open("./input.txt")
+    end
 
-def calculate_fuel(mass)
-  (mass/3).floor - 2
+    def calculate_fuel(mass)
+      (mass/3).floor - 2
+    end
+
+    def part_one
+      masses = get_data
+      masses.map(&:to_i).sum { |mass| calculate_fuel(mass) }
+    end
+
+    def part_two
+      masses = get_data.map(&:to_i)
+      fuel_reqs = []
+      masses.each do |mass|
+        total_fuel = 0
+        while mass > 0
+          mass = calculate_fuel(mass)
+          total_fuel += mass if mass > 0
+        end
+        fuel_reqs << total_fuel
+      end
+      fuel_reqs.sum
+    end
+  end
 end
-
-fuel_reqs = []
-
-input_data.each do |m|
-  fuel_reqs << calculate_fuel(m.to_i)
-end
-
-total_fuel = fuel_reqs.inject(:+)
-
-# part 2
